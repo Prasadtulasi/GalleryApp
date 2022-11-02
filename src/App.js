da7d08d5b9501f4af33d7aae48b6bab876dd6739
Prasadtulasi <sivaprasadtulasi480@gmail.com>
2022-11-02 12:59:07 +0530

    developent commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Pictures from './components/Pictures';
+
+function App() {
+  return (
+    <div className="App">
+      <Pictures />
+    </div>
+  );
+}
+
+export default App;
